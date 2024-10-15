3b9bed88c89b28b8b71876857862614a84c7b835
B3-Prompt-Eng <promptenb3@gmail.com>
2024-10-15 16:58:32 +0700

    Update app.py

--- Program/app.py
+++ Program/app.py
@@ -139,7 +139,7 @@ def process_data(sale_file, customer_file):
         eval_metrics = ['accuracy', 'accuracy']  # metrics used to evaluate predictions for each label (optional)
         
         multi_predictor = MultilabelPredictor(labels=labels, problem_types=problem_types, eval_metrics=eval_metrics)
-        predictor = multi_predictor.load(r"P1_Models_New1")
+        predictor = multi_predictor.load(r"P1_Models_New")
         predictions = predictor.predict(merged_df.drop(['Customer ID'], axis=1))
         
         merged_df["Credit Term(Day)"] = predictions["Credit Term(Day)"]
